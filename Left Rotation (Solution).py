83e3bd59bd74209fc0da7e13a05679b2a51bf103
Roberto <roberto.perezb@outlook.com>
2019-03-15 23:54:51 -0600

    Left Rotation (Solution)

---
+++ Left Rotation (Solution).py
@@ -0,0 +1,37 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#esta funcion recibe la lista y las n rotaciones
+#la funcion crea dos listas, usando "slicing" y
+#regresa la concatenacion de ambas listas
+def rotate(l, n):
+    return l[-n:] + l[:-n]
+
+
+
+def leftRotations(numbers,size,nRotations):
+	if(size == nRotations):
+		return numbers
+	else:
+		rotations = size % nRotations
+		l = rotate(numbers,rotations)
+		return l
+
+
+
+
+io = input();
+
+arraySize,dLeftRotations = io.split(" ")
+arraySize = int(arraySize)
+dLeftRotations = int(dLeftRotations)
+
+array = input()
+numbers = [int(d) for d in re.findall(r'-?\d+', array)]
+
+print(str(leftRotations(numbers,arraySize,dLeftRotations)))

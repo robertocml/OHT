6ee569933f8bea5f980a06f95cd3efe7a507bdb2
Roberto <roberto.perezb@outlook.com>
2019-12-06 00:39:32 -0600

    Update tcp_client.py

--- tcp_client.py
+++ tcp_client.py
@@ -1,1 +1,17 @@
 import socket
+
+#Example TCP Client
+
+HOST = '127.0.0.1' 
+PORT = 65432 
+
+
+#AF_NET -> para IPv4 estandar
+#SOCK_STREAM -> para indicar que es un cliente TCP
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    output = "Correcto"
+    s.sendall(b'Correcto')
+    data = s.recv(1024)
+
+print('Received', repr(data))
